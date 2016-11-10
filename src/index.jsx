b09186e7c57450a16d1a1d386128bb07154dade9
Christopher Varakliotis <c.varakliotis@gmail.com>
2016-11-10 15:44:20 +0200

    4 - Refactoring the Entry Point

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,16 @@
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import todoApp from './reducers';
-import App from './components/App';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
-const store = createStore(
-	todoApp,
-	persistedState
-);
-
-store.subscribe(throttle(() => {
-	saveState({
-		todos: store.getState().todos
-	});
-}, 1000));
+const store = configureStore();
 
 // Provider component takes the store as a prop
 // and stores it in context, making it available
 // to any components that wish to connect to it
 // (see connect() from 'react-redux')
 render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<Root store={store} />,
 	document.getElementById('app')
 );
