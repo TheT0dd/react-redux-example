f389fd37ede9e371eb9162b17d630c3c47428963
Christopher Varakliotis <c.varakliotis@gmail.com>
2016-11-11 10:01:15 +0200

    7 - Filtering Redux State with React Router Params

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,6 +3,10 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
+// Provider component takes the store as a prop
+// and stores it in context, making it available
+// to any components that wish to connect to it
+// (see connect() from 'react-redux')
 const Root = ({ store }) => (
 	<Provider store={store}>
 		<Router history={browserHistory}>
--- src/index.jsx
+++ src/index.jsx
@@ -6,10 +6,6 @@ import Root from './components/Root';
 
 const store = configureStore();
 
-// Provider component takes the store as a prop
-// and stores it in context, making it available
-// to any components that wish to connect to it
-// (see connect() from 'react-redux')
 render(
 	<Root store={store} />,
 	document.getElementById('app')
