9b4d6ac10c8b97b036eef492ac547d515d96f68c
Christopher Varakliotis <c.varakliotis@gmail.com>
2016-11-11 10:35:12 +0200

    9 - Using mapDispatchToProps() Shorthand Notation

--- src/components/VisibleTodoList.jsx
+++ src/components/VisibleTodoList.jsx
@@ -25,11 +25,11 @@ const mapStateToProps = (state, { params }) => ({
 	)
 });
 
-const mapDispatchToProps = (dispatch) => ({
-	onTodoClick(id) {
-		dispatch(toggleTodo(id));
-	}
-});
+// const mapDispatchToProps = (dispatch) => ({
+// 	onTodoClick(id) {
+// 		dispatch(toggleTodo(id));
+// 	}
+// });
 
 // Generate container component VisibleTodoList
 // that is the result of wrapping Todo list twice
@@ -38,7 +38,12 @@ const mapDispatchToProps = (dispatch) => ({
 // 2. withRouter() -> stores route `params` in (own) props
 const VisibleTodoList = withRouter(connect(
 	mapStateToProps,
-	mapDispatchToProps
+	// mapDispatchToProps() shorthand:
+	// onTodoClick() will be a function that will call
+	// toggleTodo() to generate an action passing through
+	// its args in the same order and then call dispatch()
+	// to dispatch the action to the store
+	{ onTodoClick: toggleTodo }
 )(TodoList));
 
 export default VisibleTodoList;
