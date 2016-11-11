032656bc5b3e36b02607835bff806c1ab05d40ec
Christopher Varakliotis <c.varakliotis@gmail.com>
2016-11-11 13:26:51 +0200

    11 - Normalizing the State Shape

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,52 +1,51 @@
-const todo = (state, action) => {
+import {combineReducers} from 'redux';
+import todo from './todo';
+
+const byId = (state = {}, action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
-			return {
-				id: action.id,
-				text: action.text,
-				completed: false
-			};
 		case 'TOGGLE_TODO':
-			if (state.id !== action.id) {
-				return state;
-			}
 			return {
 				...state,
-				completed: !state.completed
+				[action.id]: todo(state[action.id], action)
 			};
 		default:
 			return state;
 	}
 };
 
-const todos = (state = [], action) => {
+const allIds = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
-			return [
-				...state,
-				todo(undefined, action)
-			];
-		case 'TOGGLE_TODO':
-			return state.map(t => todo(t, action));
+			return [...state, action.id];
 		default:
 			return state;
 	}
 };
+
+const todos = combineReducers({
+	byId,
+	allIds
+});
 
 export default todos;
 
 // Selectors
 // ---
-// Named exports starting with get that select things
+// Functions starting with get that select things
 // from the current state. state refers to the todos
-// array (just like the state in todos reducer)
+// object (just like the state in todos reducer)
+const getAllTodos = (state) =>
+	state.allIds.map(id => state.byId[id]);
+
 export const getVisibleTodos = (state, filter) => {
+	const allTodos = getAllTodos(state);
 	switch (filter) {
 		case 'all':
-			return state;
+			return allTodos;
 		case 'completed':
-			return state.filter(t => t.completed);
+			return allTodos.filter(t => t.completed);
 		case 'active':
-			return state.filter(t => !t.completed);
+			return allTodos.filter(t => !t.completed);
 	}
 };
