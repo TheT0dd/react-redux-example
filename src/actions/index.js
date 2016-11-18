c310a1a22b6e46a7e6efff76865d66fa4b3ba5ad
Christopher Varakliotis <c.varakliotis@gmail.com>
2016-11-18 15:00:27 +0200

    24 - Displaying Error Messages

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,19 +2,8 @@ import { v4 } from 'node-uuid';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
-const requestTodos = (filter) => ({
-	type: 'REQUEST_TODOS',
-	filter: filter
-});
-
-const receiveTodos = (filter, response) => ({
-	type: 'RECEIVE_TODOS',
-	filter,
-	response
-});
-
 // Action creator that returns a "thunk".
-// A thunk is function returned from another function
+// A thunk is a function returned from another function
 // and in this case we use thunks as a means of
 // dispatching multiple times during the course of
 // a single action.
@@ -28,8 +17,24 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 	if (getIsFetching(getState(), filter)) {
 		return Promise.resolve();
 	}
-	dispatch(requestTodos(filter));
-	return api.fetchTodos(filter).then(response => {
-		dispatch(receiveTodos(filter, response));
+	dispatch({
+		type: 'FETCH_TODOS_REQUEST',
+		filter
 	});
+	return api.fetchTodos(filter).then(
+		response => {
+			dispatch({
+				type: 'FETCH_TODOS_SUCCESS',
+				filter,
+				response
+			});
+		},
+		error => {
+			dispatch({
+				type: 'FETCH_TODOS_FAILURE',
+				filter,
+				message: error.message || 'Something went wrong.'
+			});
+		}
+	);
 };
